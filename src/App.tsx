2f5688cd0eb87ea19c474f2b7a2590d098691ba9
GRAM <jgseo200par@naver,com>
2024-10-07 17:29:37 +0900

    ✨ 1007commit
    1007comiit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+
+import Text from '@shared/Text';
+import Button from '@shared/Button';
+import Input from '@shared/Input';
+import TextField from '@shared/TextField';
+import Alert from '@shared/Alert';
+import { useAlertContext } from '@contexts/AlertContext';
 
 function App() {
+  const { open } = useAlertContext();
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          MCard <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Text typography="t1" display="block" color="red">
+        t1
+      </Text>
+      <Text typography="t2" color="blue">
+        t2
+      </Text>
+      <Text typography="t3">t3</Text>
+      <Text typography="t4">t4</Text>
+      <Text typography="t5">t5</Text>
+
+      <div style={{ height: 10, width: '100%', background: '#efefef' }}></div>
+      <Button>클릭해주세요</Button>
+      <Button color="success">클릭해주세요</Button>
+      <Button color="error" weak={true}>
+        클릭해주세요
+      </Button>
+      <Button>클릭해주세요</Button>
+      <Button>클릭해주세요</Button>
+
+      <Input placeholder="로그인" aria-invalid={false} />
+      <Input aria-invalid={true} />
+
+      <TextField label="아이디" />
+      <TextField label="패스워드" hasError={true} />
+      {/* 
+      <Alert
+        open={true}
+        description="Hello"
+        title="알럿이 떴습니다"
+        onButtonClick={() => {}}
+      /> */}
+
+      <Button
+        onClick={() => {
+          open({
+            title: '카드신청완료',
+            description: '내역페이지에서 확인해주세요',
+            onButtonClick: () => {},
+          });
+        }}
+      >
+        Alert 오픈
+      </Button>
     </div>
   );
 }
